ae023fd19a9997632ee675f466adcb8e0b8abf9b
Yuze Jiang <fortissimoX@gmail.com>
2017-07-12 15:40:14 +0800

    Use MediaPlayer framework only on 10.13+
    Connect settings for 10.13+

--- iina/AppDelegate.swift
+++ iina/AppDelegate.swift
@@ -81,16 +81,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     if #available(macOS 10.12.2, *) {
       NSApp.isAutomaticCustomizeTouchBarMenuItemEnabled = false
       NSWindow.allowsAutomaticWindowTabbing = false
+    }
 
-      let remoteCommand = MPRemoteCommandCenter.shared()
-      remoteCommand.playCommand.addTarget(handler: PlayerCore.handlePlayCommand(_:))
-      remoteCommand.pauseCommand.addTarget(handler: PlayerCore.handlePauseCommand(_:))
-      remoteCommand.togglePlayPauseCommand.addTarget(handler: PlayerCore.handleTogglePlayPauseCommand(_:))
-      remoteCommand.stopCommand.addTarget(handler: PlayerCore.handleStopCommand(_:))
-      remoteCommand.nextTrackCommand.addTarget(handler: PlayerCore.handleNextTrackCommand(_:))
-      remoteCommand.previousTrackCommand.addTarget(handler: PlayerCore.handlePreviousTrackCommand(_:))
-
-      NowPlayingInfoManager.updateState(.unknown)
+    if #available(macOS 10.13, *) {
+      RemoteCommandController.useSystemMediaControl = UserDefaults.standard.bool(forKey: Preference.Key.useMediaKeys)
+      if RemoteCommandController.useSystemMediaControl {
+        RemoteCommandController.setup()
+        NowPlayingInfoManager.updateState(.unknown)
+      }
     }
 
     // pending open request
@@ -305,4 +303,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     Utility.setSelfAsDefaultForAllFileTypes()
   }
 
+}
+
+
+@available(OSX 10.13, *)
+class RemoteCommandController {
+  static let remoteCommand = MPRemoteCommandCenter.shared()
+
+  static var useSystemMediaControl: Bool = false
+
+  static func setup() {
+    remoteCommand.playCommand.addTarget(handler: PlayerCore.handlePlayCommand(_:))
+    remoteCommand.pauseCommand.addTarget(handler: PlayerCore.handlePauseCommand(_:))
+    remoteCommand.togglePlayPauseCommand.addTarget(handler: PlayerCore.handleTogglePlayPauseCommand(_:))
+    remoteCommand.stopCommand.addTarget(handler: PlayerCore.handleStopCommand(_:))
+    remoteCommand.nextTrackCommand.addTarget(handler: PlayerCore.handleNextTrackCommand(_:))
+    remoteCommand.previousTrackCommand.addTarget(handler: PlayerCore.handlePreviousTrackCommand(_:))
+  }
 }
